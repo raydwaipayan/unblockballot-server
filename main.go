9f3a8ca84931c91f71a6a448beb3b1e5782119f4
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-11-06 19:36:18 +0530

    allow cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	router "github.com/raydwaipayan/unblockballot-server/router"
@@ -10,6 +11,10 @@ import (
 func main() {
 	app := fiber.New()
 	app.Use(recover.New(), logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	router.SetupRoutes(app)
 	app.Listen(":3000")
