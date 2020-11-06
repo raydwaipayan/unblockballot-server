52162af18244e4c8ce3a4690026c7ce3afbe7538
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-11-06 21:01:07 +0530

    Add database schema

--- migrations/1_init.go
+++ migrations/1_init.go
@@ -8,12 +8,37 @@ import (
 
 func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
-		fmt.Println("creating table my_table...")
-		_, err := db.Exec(`CREATE TABLE my_table()`)
+		fmt.Println("creating tables...")
+		_, err := db.Exec(`
+		CREATE TABLE organization (
+			id serial PRIMARY KEY,
+			org_name varchar(30) NOT NULL,
+			org_image bytea
+		);
+		CREATE TABLE users (
+			id serial PRIMARY KEY,
+			first_name varchar(30),
+			last_name varchar(30),
+			email varchar(50),
+			password_hash varchar,
+			role integer,
+			createdAt timestamp
+		);
+		CREATE TABLE subscriptions (
+			uid integer references organization(id),
+			oid integer references users(id),
+			CONSTRAINT PK PRIMARY KEY(uid, oid)
+		);
+		`)
+
 		return err
 	}, func(db migrations.DB) error {
-		fmt.Println("dropping table my_table...")
-		_, err := db.Exec(`DROP TABLE my_table`)
+		fmt.Println("dropping tables...")
+		_, err := db.Exec(`
+		DROP TABLE subscriptions;
+		DROP TABLE users;
+		DROP TABLE organization;
+		`)
 		return err
 	})
 }
